8d6100d7f5eee670f4607fa6f3e14905719e0aca
Shubham Giri <53148932+iamshubhamg@users.noreply.github.com>
2020-08-13 20:44:42 +0530

    Added the Temp Location JavaScript Code

---
+++ Tasks/Task 20/helpers.js
@@ -0,0 +1,18 @@
+const createVenueHTML = (name, location, iconSource) => {
+  return `<h2>${name}</h2>
+  <img class="venueimage" src="${iconSource}"/>
+  <h3>Address:</h3>
+  <p>${location.address}</p>
+  <p>${location.city}</p>
+  <p>${location.country}</p>`;
+}
+
+const createWeatherHTML = (currentDay) => {
+  console.log(currentDay)
+  return `<h2>${weekDays[(new Date()).getDay()]}</h2>
+		<h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
+		<h2>Condition: ${currentDay.weather[0].description}</h2>
+  	<img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
+}
+
+const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
